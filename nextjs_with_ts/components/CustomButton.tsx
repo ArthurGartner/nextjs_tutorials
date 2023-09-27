30c48c6ccc9e51e6beb117f463ce403c799b48b4
Arthur Gartner <Arthur.Gartner@gmail.com>
2023-09-26 20:57:44 -0600

    Started work on new tutorial

--- nextjs_with_ts/components/CustomButton.tsx
+++ nextjs_with_ts/components/CustomButton.tsx
@@ -8,15 +8,16 @@ const CustomButton = ({
   title,
   containerStyles,
   handleClick,
+  btnType,
 }: CustomButtonProps) => {
   return (
     <button
       disabled={false}
-      type={"button"}
-      className={`custom-btn`}
+      type={btnType || "button"}
+      className={`custom-btn ${containerStyles}`}
       onClick={() => {}}
     >
-      <span className={`flex-1`}>Title</span>
+      <span className={`flex-1`}>{title}</span>
     </button>
   );
 };
