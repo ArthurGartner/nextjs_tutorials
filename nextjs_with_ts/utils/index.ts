a01e907d4a79576b79d0b86efa0544a7b216c4b6
Arthur Gartner <Arthur.Gartner@gmail.com>
2023-10-01 13:43:20 -0600

    Additional tutorial work. Removed exposed API key

--- nextjs_with_ts/utils/index.ts
+++ nextjs_with_ts/utils/index.ts
@@ -1,11 +1,13 @@
+import { CarProps } from "@/types";
+
 export async function fetchCars() {
   const headers = {
-    "X-RapidAPI-Key": "d610a7ad20msh61ad816a42bbe6ap155dbbjsnba7310efcf6a",
+    "X-RapidAPI-Key": process.env.RAPID_CARS_API_KEY as string,
     "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
   };
 
   const response = await fetch(
-    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla",
+    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=carrera",
     {
       headers: headers,
     }
@@ -29,4 +31,20 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
   const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
 
   return rentalRatePerDay.toFixed(0);
+};
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+  const url = new URL("https://cdn.imagin.studio/getimage");
+
+  const { make, year, model } = car;
+
+  url.searchParams.append("customer", "hrjavascript-mastery");
+
+  url.searchParams.append("make", make);
+  url.searchParams.append("modelFamily", model.split(" ")[0]);
+  url.searchParams.append("zoomType", "fullscreen");
+  url.searchParams.append("modelYear", `${year}`);
+  url.searchParams.append("angle", `${angle}`);
+
+  return `${url}`;
 };
